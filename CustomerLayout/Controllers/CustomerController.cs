c84ba205372d7755d6041783889186c54aea4e40
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-01 15:56:08 +0530

    Add project files.

---
+++ CustomerLayout/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerLayout.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.msg = "welcome to Customer Page";
+            return View();
+        }
+
+        public ActionResult CustList()
+        {
+            List<string> ll = new List<string>()
+        {
+            "Ramesh","Suresh","Naresh"
+        };
+            ViewBag.CustList = ll;
+            return View();
+        }
+    }
+}
